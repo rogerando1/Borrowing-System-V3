1f3143f9fcb7413a85fced41e5405be07f091b93
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-05-25 14:51:58 +0800

    Final setup

--- Borrowing System/Borrowing System/Data/DatabaseConfig.cs
+++ Borrowing System/Borrowing System/Data/DatabaseConfig.cs
@@ -12,16 +12,16 @@ namespace Borrowing_System.Data
 
         //LOCAL HOST
 
-        //public static string ServerName = "localhost";
-        //public static string UserId = "root";
-        //public static string Password = "root";
-        //public static string DatabaseName = "borrowingsystem";
+        public static string ServerName = "localhost";
+        public static string UserId = "root";
+        public static string Password = "root";
+        public static string DatabaseName = "borrowingsystem";
 
         //SQL FREE  HOSTING
 
-        public static string ServerName = "sql6.freemysqlhosting.net";
-        public static string UserId = "sql6690575";
-        public static string Password = "WzrG9YgeeE";
-        public static string DatabaseName = "sql6690575";
+        //public static string ServerName = "sql6.freemysqlhosting.net";
+        //public static string UserId = "sql6690575";
+        //public static string Password = "WzrG9YgeeE";
+        //public static string DatabaseName = "sql6690575";
     }
 }
